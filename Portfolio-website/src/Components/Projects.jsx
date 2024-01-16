c9554b50e06a0cf34634a83a7d7b66ced599af00
whitworthjoe <whitworthjoe02@gmail.com>
2024-01-16 18:55:55 +0000

    grid setup + container project section

--- Portfolio-website/src/Components/Projects.jsx
+++ Portfolio-website/src/Components/Projects.jsx
@@ -6,8 +6,24 @@ const Projects = () => {
     <div className="projectsContainer">
       <h1 className="projectsHeader">My Recent Projects</h1>
       <div className="projects">
-        <h2>A 2-player, full-stack browser card game, inspired by the classic card game, Jaipur.</h2>
-        <h2>Dynamic web application designed to deliver news articles with a focus on community interaction.</h2>
+        <div className="projectBox">
+          <h1>Resource Rivals</h1>
+          <h2>
+            A 2-player, full-stack browser card game, inspired by the classic
+            card game Jaipur.
+          </h2>
+        </div>
+        <div className="projectBox">
+          <h1>Crowley's News Back-End</h1>
+          <h2>Crafting the Database Magic: SQL/PSQL</h2>
+        </div>
+        <div className="projectBox">
+          <h1>Crowley's News Front-End</h1>
+          <h2>
+            Dynamic web application designed to deliver news articles with a
+            focus on community interaction.
+          </h2>
+        </div>
       </div>
     </div>
   );
